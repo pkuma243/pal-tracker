2861fb2c5b8b7fc34cb0a5bcd3fffb80d3ee1b06
priyendu <priyendu.kumar@wipro.com>
2019-09-11 02:13:13 -0700

    lab 6

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,7 +1,12 @@
 package io.pivotal.pal.tracker;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -9,10 +14,24 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-    @Bean
+    /*@Bean
     public TimeEntryRepository getTimeEntryRepository(){
         final TimeEntryRepository inMemoryTimeEntryRepository = new InMemoryTimeEntryRepository();
         return inMemoryTimeEntryRepository;
-    }
+    }*/
+   /* @Bean
+    public TimeEntryRepository getTimeEntryRepository(){
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.execute("TRUNCATE time_entries");
 
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        final JdbcTimeEntryRepository jdbcTimeEntryRepository = new JdbcTimeEntryRepository(dataSource);
+        return jdbcTimeEntryRepository;
+    }*/
+    @Bean
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
+    }
 }
